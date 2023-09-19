c2042f5d191ebedcdb4d93da70878da7f6988c26
Jeong Hyeon <da864268@naver.com>
2023-09-19 16:14:01 +0900

    feat: 블루투스로 사용자 식별 기능 추가

---
+++ app/src/main/java/com/example/marsproject/BluetoothSearch.kt
@@ -0,0 +1,88 @@
+/*블루투스 사용을 위한 클래스*/
+package com.example.marsproject
+
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.AdvertiseCallback
+import android.bluetooth.le.AdvertiseData
+import android.bluetooth.le.AdvertiseSettings
+import android.bluetooth.le.BluetoothLeAdvertiser
+import android.bluetooth.le.BluetoothLeScanner
+import android.os.Looper
+import android.os.ParcelUuid
+import android.util.Log
+import androidx.core.os.HandlerCompat
+
+// 권한 오류 방지
+@SuppressLint("MissingPermission")
+class BluetoothSearch(bluetoothManager: BluetoothManager) {
+    private val mainThreadHandler = HandlerCompat.createAsync(Looper.getMainLooper())
+    private val advertiser: BluetoothLeAdvertiser
+    private val scanner: BluetoothLeScanner
+    private var isDiscoverable = false // 블루투스 검색 가능 모드 상태
+    private var isScan = false // 블루투스 스캔 모드 상태
+    var bluetoothAdapter: BluetoothAdapter
+
+    // 블루투스 초기화
+    init {
+        bluetoothAdapter = bluetoothManager.adapter
+        advertiser = bluetoothAdapter.bluetoothLeAdvertiser
+        scanner = bluetoothAdapter.bluetoothLeScanner
+    }
+
+    // 다른 장치가 식별 할 수 있도록 검색 가능 모드를 켜주는 함수
+    fun BluetoothDiscoverable(randomuuid: ParcelUuid) {
+        if (isDiscoverable) {
+            Log.d("블루투스", "이미 검색 가능 모드임")
+            return
+        }
+        isDiscoverable = true
+        val settings = AdvertiseSettings.Builder()
+            .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
+            .setConnectable(true)
+            .setTimeout(0)
+            .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+            .build()
+        // 광고모드 설정
+        val data = AdvertiseData.Builder()
+            .addServiceUuid(randomuuid)
+            .build()
+        // 보낼 데이터
+
+        // 검색 가능 모드 시작과 결과 리턴
+        advertiser.startAdvertising(settings, data, object : AdvertiseCallback() {
+            override fun onStartSuccess(settingsInEffect: AdvertiseSettings?) {
+                Log.d("블루투스", "성공적으로 광고")
+                super.onStartSuccess(settingsInEffect)
+            }
+
+            override fun onStartFailure(errorCode: Int) {
+                Log.d("블루투스", "광고 실패 code: $errorCode")
+                super.onStartFailure(errorCode)
+            }
+        })
+    }
+
+    // 블루투스로 유저 찾기 시작
+    fun startbluetoothSearch(minute: Int): Boolean {
+        if (!isScan) {
+            // 중복 스캔이 아닌경우
+            val scanval = SearchPeopleActivity().bluetoothSearchCallback
+            isScan = true
+            scanner.startScan(scanval)
+            Log.d("블루투스", "스캔이 시작됨")
+
+            mainThreadHandler.postDelayed({
+                scanner.stopScan(scanval)
+                isScan = false
+                Log.d("블루투스", "스캔이 종료됨")
+            }, (minute * 60 * 1000).toLong())
+
+            return true
+        } else {
+            // 중복 스캔일 경우
+            return false
+        }
+    }
+}
--- app/src/main/java/com/example/marsproject/SearchPeopleActivity.kt
+++ app/src/main/java/com/example/marsproject/SearchPeopleActivity.kt
@@ -1,13 +1,24 @@
 package com.example.marsproject
 
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.marsproject.databinding.ActivitySearchPeopleBinding
 
+@SuppressLint("MissingPermission")
 class SearchPeopleActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySearchPeopleBinding
+    private lateinit var bluetoothManager: BluetoothManager
+    private lateinit var bluetoothsearch: BluetoothSearch
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchPeopleBinding.inflate(layoutInflater)
@@ -18,15 +29,65 @@ class SearchPeopleActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.icon_left_resize)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
+        bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothsearch = BluetoothSearch(bluetoothManager)
         // 사용자 찾기 버튼 클릭 리스너
-        binding.searchButton.setOnClickListener{
-            // 추후 ar 기능으로 이동 작성
-            Toast.makeText(applicationContext, "ar 기능 준비중", Toast.LENGTH_SHORT).show()
+        binding.searchButton.setOnClickListener {
+            if (!bluetoothsearch.bluetoothAdapter.isEnabled) {
+                // 블루투스 활성화 안될때 활성화 시키기
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                resultLaunch.launch(enableBtIntent) // 이 함수를 사용하면 resultLaunch 메소드가 실행됨
+            } else {
+                // 2분동안 다른 블루투스 장치를 찾음
+                if (bluetoothsearch.startbluetoothSearch(2)) {
+                    Toast.makeText(applicationContext, "다른 플레이어를 찾는중....", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    // 블루투스가 활성화 되어있지 않을때 사용자로 부터 요청을 받음
+    val resultLaunch = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        // 사용자가 블루투스 사용을 허용 했을때
+        if (result.resultCode == -1) {
+            // 2분동안 다른 블루투스 장치를 찾음
+            if (bluetoothsearch.startbluetoothSearch(2)) {
+                Toast.makeText(applicationContext, "다른 플레이어를 찾는중....", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    val bluetoothSearchCallback = object : ScanCallback() {
+        private val isdup = ArrayList<String>() // 장치 중복 제거용 ArrayList
+
+        // 블루투스가 장치를 찾을 때 마다 해당 함수를 실행함(비동기임)
+        override fun onScanResult(callbackType: Int, result: ScanResult?) {
+            super.onScanResult(callbackType, result)
+
+            val name = result?.device?.name
+            val uuid = result?.scanRecord?.serviceUuids?.get(0).toString() // 이 uuid를 db에서 스켄 해야함
+            val mac = result?.device?.address
+
+            // 장치 검색 결과 중복 제거 및 uuid null 로 나오는거 제거
+            if (!isdup.contains(mac.toString()) && uuid != "null") {
+                isdup.add(mac.toString())
+
+                // TODO: 이제 여기다 블루투스 장치를 찾을때 마다 api /getbtuserdata/[uuid] 에서 uuid 보내서 유저 정보 얻는거 만들면 됨
+                // mac 주소를 보내고 싶으나 안드로이드 보안 정책상 자기 블루트스 mac 주소를 얻을 수 없음
+                // 그래서 로그인 할 때 랜덤 UUID 를 발급하여 그걸로 장치를 식별 하기로 함
+
+                Log.d("로그", "장치이름 : $name")
+                // Advertiser의 31 바이트 제한으로 장치이름은 보내지 않음 즉 null로 표시
+                Log.d("로그", "mac 주소 : $mac")
+                Log.d("로그", "UUID : $uuid")
+                // 일반적인 블루투스장치의 경우 uuid는 null로 뜰꺼임
+                // 앱을 킨 사람은 ble로 uuid를 넣어서 계속 해서 신호를 보니기 때문에 정상적으로 출력 될꺼임
+            }
         }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item?.itemId){
+        when (item?.itemId) {
             android.R.id.home -> { // 뒤로 가기 버튼 눌렀을 때
                 finish()
             }
