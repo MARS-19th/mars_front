6675fb256ebb1f06a167d40c1ea63e670773aade
Lseoksee <da864268@naver.com>
2023-09-22 21:42:43 +0900

    feat: 카카오 계정 연동 해제 일부 구현

--- app/src/main/java/com/example/marsproject/MainMypageFragment.kt
+++ app/src/main/java/com/example/marsproject/MainMypageFragment.kt
@@ -1,12 +1,15 @@
 package com.example.marsproject
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.marsproject.databinding.FragmentMainMypageBinding
+import com.kakao.sdk.user.UserApiClient
 
 class MainMypageFragment : Fragment() {
     private lateinit var binding: FragmentMainMypageBinding
@@ -19,6 +22,16 @@ class MainMypageFragment : Fragment() {
 
         // 클릭 시 로그아웃 및 화면 전환 리스너
         binding.logoutButton.setOnClickListener{
+            // 카카오 계정 연동 해제
+            UserApiClient.instance.logout { err ->
+                if (err == null) {
+                    Log.d(ContentValues.TAG, "로그아웃 성공")
+                }
+                else {
+                    Log.d(ContentValues.TAG, "로그아웃 실패")
+                }
+            }
+
             (activity as MainActivity).clearLogin() // 로그인 정보 삭제
             (activity as MainActivity).clearName() // 닉네임 정보 삭제
             (activity as MainActivity).clickchangeFragment(3) // 홈 프래그먼트로 전환
