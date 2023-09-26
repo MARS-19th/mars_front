add6e7e65e15850e56f9d43fe68b0d27359e2188
hohyun <hohyun1204@naver.com>
2023-09-26 10:09:57 +0900

    feat: 디자인 변경에 따른 레이아웃 및 코드 수정

--- app/src/main/java/com/example/marsproject/StoreActivity.kt
+++ app/src/main/java/com/example/marsproject/StoreActivity.kt
@@ -1,8 +1,10 @@
 package com.example.marsproject
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import com.example.marsproject.databinding.ActivityStoreBinding
 
 class StoreActivity : AppCompatActivity() {
@@ -17,8 +19,39 @@ class StoreActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar) // 툴바 지정
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // 뒤로가기 버튼 활성화
         supportActionBar?.setHomeAsUpIndicator(R.drawable.icon_left_resize) // 뒤로가기 버튼 이미지 설정
-        supportActionBar?.setDisplayShowTitleEnabled(false) // 앱 타이틀 비활성화
+        supportActionBar?.title = "상점" // 타이틀 지정
 
+        // 카테고리 클릭 리스너 설정
+        categoryClickListener()
+    }
+
+    // 카테고리 클릭 리스너 설정
+    private fun categoryClickListener(){
+        // 클릭 시 해당 뷰 밑줄 색상 변경 리스너
+        val clklistener = View.OnClickListener {
+            when(it.id) {
+                R.id.headImage -> {
+                    binding.headView.setBackgroundColor(Color.parseColor("#FF9C46")) // 머리 밑줄 색상 변경
+                    binding.bodyView.setBackgroundColor(Color.parseColor("#DDDDDD")) // 몸통 밑줄 색상 변경
+                    binding.etcView.setBackgroundColor(Color.parseColor("#DDDDDD")) // 기타 밑줄 색상 변경
+                }
+                R.id.bodyImage -> {
+                    binding.headView.setBackgroundColor(Color.parseColor("#DDDDDD")) // 머리 밑줄 색상 변경
+                    binding.bodyView.setBackgroundColor(Color.parseColor("#FF9C46")) // 몸통 밑줄 색상 변경
+                    binding.etcView.setBackgroundColor(Color.parseColor("#DDDDDD")) // 기타 밑줄 색상 변경
+                }
+                R.id.etcImage -> {
+                    binding.headView.setBackgroundColor(Color.parseColor("#DDDDDD")) // 머리 밑줄 색상 변경
+                    binding.bodyView.setBackgroundColor(Color.parseColor("#DDDDDD")) // 몸통 밑줄 색상 변경
+                    binding.etcView.setBackgroundColor(Color.parseColor("#FF9C46")) // 기타 밑줄 색상 변경
+                }
+            }
+        }
+
+        // 클릭 리스너 지정
+        binding.headImage.setOnClickListener(clklistener)
+        binding.bodyImage.setOnClickListener(clklistener)
+        binding.etcImage.setOnClickListener(clklistener)
     }
 
     // 옵션 메뉴 클릭 함수
