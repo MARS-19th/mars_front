1267c1ffe188a2e91c2141d80e36336e5f42e2b8
hohyun <hohyun1204@naver.com>
2023-10-10 21:47:09 +0900

    fix: 기존 프로필 사진 남아있는 현상 수정

--- app/src/main/java/com/example/marsproject/MainHomeFragment.kt
+++ app/src/main/java/com/example/marsproject/MainHomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.marsproject.databinding.FragmentMainHomeBinding
 import java.net.UnknownServiceException
@@ -79,6 +80,11 @@ class MainHomeFragment : Fragment() {
 
         // 유저 데이터 변경
         changeUserData(title, name, id, objective, life, progress.toInt())
+
+        // 기존 프로필 사진 디스크 케쉬 지우기
+        Thread {
+            Glide.get(requireActivity()).clearDiskCache()
+        }.start()
 
         // 서버에서 프사 이미지 가져와서 profileImage에 적용하기
         Glide.with(this)
