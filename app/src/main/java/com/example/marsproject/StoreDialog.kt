31c4572ba21e5c5539a0d867d503a39aa215c257
hohyun <hohyun1204@naver.com>
2023-10-13 02:32:14 +0900

    feat: 아이템 구매 시 재화가 부족하면 Toast 메시지 출력

--- app/src/main/java/com/example/marsproject/StoreDialog.kt
+++ app/src/main/java/com/example/marsproject/StoreDialog.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.Window
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.marsproject.databinding.StoreDialogBinding
 import org.json.JSONObject
@@ -59,64 +60,66 @@ class StoreDialog(
             // 여기에서 구매하기 버튼을 눌렀을 때 수행할 동작
             // (http://dmumars.kro.kr/api/setuserinventory)
 
-            //아이템 인벤토리에 유저네임, object id 디비에 저장
-            Thread {
-                try {
-                    val outputjson = JSONObject() //json 생성
-                    outputjson.put("user_name", userName) //getName()
-                    outputjson.put("object_id", id)
+            if(minji >= 0) {
+                //아이템 인벤토리에 유저네임, object id 디비에 저장
+                Thread {
+                    try {
+                        val outputjson = JSONObject() //json 생성
+                        outputjson.put("user_name", userName) //getName()
+                        outputjson.put("object_id", id)
 
-                    Request().reqpost("http://dmumars.kro.kr/api/setuserinventory", outputjson)
-                } catch (e: UnknownServiceException) {
-                    println(e.message)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }.start()
+                        Request().reqpost("http://dmumars.kro.kr/api/setuserinventory", outputjson)
+                    } catch (e: UnknownServiceException) {
+                        println(e.message)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }.start()
 
-            //현재 가지고 있는 코인에서 선택한 아이템을 사면 현재 코인-아이템 가격 값으로 바뀜
-            Thread {
-                try {
-                    val outputjson = JSONObject() //json 생성
-                    outputjson.put("user_name", userName) //getName()
-                    outputjson.put("value", minji)
+                //현재 가지고 있는 코인에서 선택한 아이템을 사면 현재 코인-아이템 가격 값으로 바뀜
+                Thread {
+                    try {
+                        val outputjson = JSONObject() //json 생성
+                        outputjson.put("user_name", userName) //getName()
+                        outputjson.put("value", minji)
 
-                    Request().reqpost("http://dmumars.kro.kr/api/setmoney", outputjson)
-                } catch (e: UnknownServiceException) {
-                    println(e.message)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-                activity.runOnUiThread {
-                    activity.load()
-                }
-            }.start()
-
-
-
-
-            // 아이템을 착용했을 때 아이템 아이디를 서버로 보내는 함수
-            Thread {
-                try {
-                    val outputjson = JSONObject() //json 생성
-                    outputjson.put("user_name", userName) //getName()
-                    outputjson.put("moun_shop", id)
-
-                    Request().reqpost("http://dmumars.kro.kr/api/setuserfititem", outputjson)
-
-                    //현재 착용 중인 아이템의 아이디를 id(object id)로 변경
-                    currentItem = id
-
-                    //미착용 버튼 클릭시 착용중으로 변경(디비에서 마운샵을 해당 아이디로 변경)
+                        Request().reqpost("http://dmumars.kro.kr/api/setmoney", outputjson)
+                    } catch (e: UnknownServiceException) {
+                        println(e.message)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                    activity.runOnUiThread {
+                        activity.load()
+                    }
+                }.start()
 
 
-                    //착용중 버튼 ㅋ틀릭시 미착용으로 변경(디비에서 마운샵을 널로 변경)
-                } catch (e: UnknownServiceException) {
-                    println(e.message)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            } .start()
+                // 아이템을 착용했을 때 아이템 아이디를 서버로 보내는 함수
+                Thread {
+                    try {
+                        val outputjson = JSONObject() //json 생성
+                        outputjson.put("user_name", userName) //getName()
+                        outputjson.put("moun_shop", id)
+
+                        Request().reqpost("http://dmumars.kro.kr/api/setuserfititem", outputjson)
+
+                        //현재 착용 중인 아이템의 아이디를 id(object id)로 변경
+                        currentItem = id
+
+                        //미착용 버튼 클릭시 착용중으로 변경(디비에서 마운샵을 해당 아이디로 변경)
+
+
+                        //착용중 버튼 ㅋ틀릭시 미착용으로 변경(디비에서 마운샵을 널로 변경)
+                    } catch (e: UnknownServiceException) {
+                        println(e.message)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }.start()
+            } else {
+                Toast.makeText(context, "재화가 부족합니다", Toast.LENGTH_SHORT).show()
+            }
 
             dlg?.dismiss()// 다이얼로그 닫기
             activity.runOnUiThread {
