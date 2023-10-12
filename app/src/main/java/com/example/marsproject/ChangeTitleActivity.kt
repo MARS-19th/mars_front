5ce9ec16d84d3e51f9622964dcc9b247251be886
hyojin0326 <114805839+hyojin0326@users.noreply.github.com>
2023-10-12 10:47:08 +0900

    ddd

--- app/src/main/java/com/example/marsproject/ChangeTitleActivity.kt
+++ app/src/main/java/com/example/marsproject/ChangeTitleActivity.kt
@@ -2,7 +2,7 @@ package com.example.marsproject
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +10,8 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import com.example.marsproject.R
 import com.example.marsproject.Request
@@ -19,17 +21,15 @@ import java.net.UnknownServiceException
 
 class ChangeTitleActivity : AppCompatActivity() {
     private lateinit var binding: ActivityChangeTitleBinding
-    private lateinit var sharedPreferences: SharedPreferences
-    private var selectedViewId: Int = 0
-    private var text: String = ""
+    //private lateinit var sharedPreferences: SharedPreferences
+    //private var selectedViewId: Int = 0 // 이전에 선택된 칭호를 나타내기 위한 변수
+    private var selectedTitle: String = ""
+    private var DBtitle: String =""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityChangeTitleBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // SharedPreferences 초기화
-        sharedPreferences = getSharedPreferences("selected_view", Context.MODE_PRIVATE)
 
         // 툴바 설정
         setSupportActionBar(binding.toolbar)
@@ -37,145 +37,19 @@ class ChangeTitleActivity : AppCompatActivity() {
         supportActionBar?.setHomeAsUpIndicator(R.drawable.icon_left_resize)
         supportActionBar?.title = "내 칭호"
 
-        // view 클릭 시 선택 기록하기 위한 변수
-        var selectedTitle: String? = null
+        //우선 다 반투명으로 하기
+        ViewOpacity(1,"")
 
-        // 프론트 뷰들
-        val frontViews =
-            arrayOf(binding.f1, binding.f2, binding.f3, binding.f4, binding.f5, binding.f6)
-        val frontTextViews =
-            arrayOf(binding.tF1, binding.tF2, binding.tF3, binding.tF4, binding.tF5, binding.tF6)
+        //DB에서 사용자의 칭호 가지고 오기
+        runOnUiThread { DBTitle() }
 
-        // 백엔드 뷰들
-        val backViews =
-            arrayOf(binding.b1, binding.b2, binding.b3, binding.b4, binding.b5, binding.b6)
-        val backTextViews =
-            arrayOf(binding.tB1, binding.tB2, binding.tB3, binding.tB4, binding.tB5, binding.tB6)
-
-        // view에 클릭 리스너 설정 함수
-        fun setClickListener(views: Array<View>, textViews: Array<TextView>) {
-            for (i in views.indices) {
-                val view = views[i]
-                val textView = textViews[i]
-
-                view.setOnClickListener {
-                    // 클릭한 뷰의 칭호를 선택하도록 기록
-                    selectedTitle = textView.text.toString()
-
-                    // 선택한 뷰의 시각적 표시 변경 (예: 배경 변경)
-                    toggleBackground(view)
-                }
-            }
-        }
-
-        // 프론트 뷰들에 클릭 리스너 설정
-        setClickListener(frontViews, frontTextViews)
-
-        // 백엔드 뷰들에 클릭 리스너 설정
-        setClickListener(backViews, backTextViews)
-
-        // "btn_check" 버튼 클릭 시 서버로 선택한 칭호를 보내는 함수
-        val btnCheck: Button = findViewById(R.id.btn_check) // "btn_check" 버튼의 ID를 설정
-        btnCheck.setOnClickListener {
-            if (selectedTitle != null) {
-                // 선택한 칭호를 서버로 전송
-                sendSelectedTitleToServer(selectedTitle!!)
-                // 버튼 배경 & 텍스트 바꾸기
-                btnCheck.setBackgroundResource(R.drawable.act_btn_check2)
-                btnCheck.text = "선택 완료"
-                btnCheck.setTextColor(Color.parseColor("#FF9C46"))
-            } else {
-                // 사용자에게 메시지 표시 또는 처리
-            }
-        }
-
-        // 이전에 선택된 뷰의 ID를 복원
-        selectedViewId = sharedPreferences.getInt("selected_view_id", 0)
-        if (selectedViewId != 0) {
-            val selectedView = findViewById<View>(selectedViewId)
-            selectedView.setBackgroundResource(R.drawable.act_btn_click)
-        }
+        //디비에서 사용자가 가지고 있는 칭호가져오기 -> 가지고 있는 칭호 불투명으롭 변경
         getSelectedTitleAndUpdateUI()
+
     }
 
-    private fun toggleBackground(view: View) {
-        if (selectedViewId != 0) {
-            // 이전에 선택된 뷰가 있으면 배경을 복원
-            val selectedView = findViewById<View>(selectedViewId)
-            selectedView.setBackgroundResource(R.drawable.act_btn)
-        }
-
-        if (selectedViewId == view.id) {
-            // 같은 뷰를 다시 클릭하면 선택 해제
-            selectedViewId = 0
-        } else {
-            // 다른 뷰를 클릭하면 배경 변경
-            view.setBackgroundResource(R.drawable.act_btn_click)
-            selectedViewId = view.id
-        }
-
-        // 선택된 뷰의 ID를 SharedPreferences에 저장
-        sharedPreferences.edit().putInt("selected_view_id", selectedViewId).apply()
-    }
-
-    fun getName(): String {
-        val pref = getSharedPreferences("userName", 0)
-        return pref.getString("name", "").toString()
-    }
-
-    //사용자가 가진 칭호 보내기
-    fun sendSelectedTitleToServer(value: String) {
-        val titleThread = Thread {
-            try {
-                val outputjson = JSONObject()
-                outputjson.put("user_name", getName()) // 사용자 이름
-                outputjson.put("value", value.replace("\n", " "))  // value 값
-
-                val jsonObject =
-                    Request().reqpost("http://dmumars.kro.kr/api/setusertitle", outputjson)
-
-                val title = jsonObject.getString("title")
-                println("사용자의 칭호: $title")
-            } catch (e: UnknownServiceException) {
-                println(e.message)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-        titleThread.start() // 쓰레드 시작
-        titleThread.join() // 쓰레드 종료될 때까지 대기
-    }
-
-    // 사용자가 가지고 있는 칭호를 가져와서 UI 업데이트
-    fun getSelectedTitleAndUpdateUI() {
-        val titleThread = Thread {
-            try {
-                val jsonObject =
-                    Request().reqget("http://dmumars.kro.kr/api/usergettitle/${getName()}")
-
-                // 사용자 칭호 추출
-                val resultsArray = jsonObject.getJSONArray("results")
-                val userTitles = mutableListOf<String>()
-
-                for (i in 0 until resultsArray.length()) {
-                    val userTitle = resultsArray.getString(i)
-                    userTitles.add(userTitle)
-                }
-
-                // UI 업데이트 함수 호출
-                updateUserTitlesUI(userTitles)
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        titleThread.start()
-    }
-
-    fun updateUserTitlesUI(userTitles: List<String>) {
-
-        // 칭호와 그에 해당하는 view를 매핑
+    //사용자 칭호를 디비에서 가지고 오기
+    fun DBTitle(){
         val titleToViewMap = mapOf(
             //프
             "초보 프냥이" to binding.vf1,
@@ -190,23 +64,179 @@ class ChangeTitleActivity : AppCompatActivity() {
             "자바스크립트 백냥이" to binding.vb3,
             "백엔드 엔지니어" to binding.vb4,
             "백엔드의 냥스터" to binding.vb5,
-            "백엔드 마에스트냥" to binding.vb6,
-
+            "백엔드 마에스트냥" to binding.vb6
         )
+        Thread {
+            try {
+                val jsonObject = Request().reqget("http://dmumars.kro.kr/api/getuserdata/${getName()}") //get요청\
+                DBtitle= jsonObject.getString("user_title") // 칭호
+                runOnUiThread{
+                    ChangeDBTitle(titleToViewMap[DBtitle] as ConstraintLayout)
+                }
+            } catch (e: UnknownServiceException) {
+                // API 사용법에 나와있는 모든 오류응답은 여기서 처리
+                println(e.message)
+                // 이미 reqget() 메소드에서 파싱 했기에 json 형태가 아닌 value 만 저장 된 상태 만약 {err: "type_err"} 인데 e.getMessage() 는 type_err만 반환
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+    private fun ChangeDBTitle(view: ConstraintLayout) {
+        val AllViews = arrayOf(binding.f1, binding.f2, binding.f3, binding.f4, binding.f5, binding.f6,
+            binding.b1, binding.b2, binding.b3, binding.b4, binding.b5, binding.b6)
 
-        //사용자 미보유 칭호
-        for (view in titleToViewMap.values) {
-            view.alpha = 0.2f // 불투명도를 0.2로 설정
+        for(a in AllViews){
+            if(a.parent==view){
+                if (a.background.constantState == ContextCompat.getDrawable(this, R.drawable.act_btn)?.constantState) {
+                    a.setBackgroundResource(R.drawable.act_btn_click)
+                    break
+                }
 
-        }
-        
-        // 사용자가 보유 칭호
-        for (userTitle in userTitles) {
-            val view = titleToViewMap[userTitle]
-            view?.alpha = 1.0f
+            }
         }
     }
 
+
+    fun ViewOpacity(num:Int, userTitle: String){
+        val titleToViewMap = mapOf(
+            //프
+            "초보 프냥이" to binding.vf1,
+            "HTML 마스터" to binding.vf2,
+            "CSS 마법사" to binding.vf3,
+            "자바스크립트 냥이" to binding.vf4,
+            "프론트엔트 냥스터" to binding.vf5,
+            "프론트엔트 마에스트냥" to binding.vf6,
+            //백
+            "초보 백엔드 냥이" to binding.vb1,
+            "백엔드 탐험가 냥이" to binding.vb2,
+            "자바스크립트 백냥이" to binding.vb3,
+            "백엔드 엔지니어" to binding.vb4,
+            "백엔드의 냥스터" to binding.vb5,
+            "백엔드 마에스트냥" to binding.vb6
+        )
+        if(num==1){
+            for (view in titleToViewMap.values) {
+                view.alpha = 0.2f // 반투명
+            }
+        }
+        else if(num==2){
+            //선택한 칭호는 불투명으로
+            runOnUiThread {
+                titleToViewMap.get(userTitle)?.alpha = 1f; //사용자가 가지고 있는 칭호창은 불투며으로 변환
+            }
+        }
+
+    }
+
+    // 사용자가 가지고 있는 칭호들를 가져와서 UI 업데이트
+    fun getSelectedTitleAndUpdateUI() {
+        val titleThread = Thread {
+            try {
+                val jsonObject = Request().reqget("http://dmumars.kro.kr/api/usergettitle/${getName()}")
+                // 사용자 칭호 추출
+                val resultsArray = jsonObject.getJSONArray("results")
+                val userTitles = mutableListOf<String>()
+
+                for (i in 0 until resultsArray.length()) {
+                    val userTitle = resultsArray.getString(i)
+                    userTitles.add(userTitle)
+
+                }
+                // 가지고 있는 칭호 불투명하게 하기
+                runOnUiThread { updateUserTitlesUI(userTitles) }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        titleThread.start()
+    }
+
+    // view에 클릭 리스너 설정 함수
+    fun setClickListener(selectedView: ConstraintLayout?, selectedText: String) {
+        selectedView?.setOnClickListener {
+            // 선택한 뷰의 시각적 표시 변경 (예: 배경 변경)
+            //toggleBackground
+            toggleBackground(selectedView)
+
+            val dlg = ChangeTitleDialog(this) // 커스텀 다이얼로그 객체 저장
+            // 예 버튼 클릭 시 실행
+            dlg.setOnOKClickedListener{
+                //사용자 칭호 보내기
+            val changeTitle = Thread {
+                val selectedTitle = selectedText
+
+                    try {
+                        val changeTitleJson = JSONObject()
+                        changeTitleJson.put("user_name", getName()) // 사용자 이름
+                        changeTitleJson.put("value", selectedTitle.replace("\n", " "))  // value 값
+
+                        Request().reqpost("http://dmumars.kro.kr/api/setusertitle", changeTitleJson)
+                    } catch (e: UnknownServiceException) {
+                        println(e.message)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+                changeTitle.start()
+                changeTitle.join()
+            }
+            Log.d("dsfdsfsfsdfs", selectedTitle)
+            // 아니오 버튼 클릭 시 실행
+            dlg.setOnNOClickedListener {}
+            dlg.show("칭호를 변경하시겠습니까?") // 다이얼로그 내용에 담을 텍스트
+        }
+    }
+    private fun toggleBackground(view: ConstraintLayout) {
+        val AllViews = arrayOf(binding.f1, binding.f2, binding.f3, binding.f4, binding.f5, binding.f6,
+            binding.b1, binding.b2, binding.b3, binding.b4, binding.b5, binding.b6)
+
+        for(a in AllViews){
+            if(a.parent==view){
+                if (a.background.constantState == ContextCompat.getDrawable(this, R.drawable.act_btn)?.constantState) {
+                    for(b in AllViews)
+                        b.setBackgroundResource(R.drawable.act_btn)
+                    a.setBackgroundResource(R.drawable.act_btn_click)
+
+                } else {
+                    a.setBackgroundResource(R.drawable.act_btn)
+                }
+                break
+            }
+        }
+    }
+
+    //내가 가지고 있는 칭호들 불투명으로 바꾸고 클릭이벤트 넣기
+    fun updateUserTitlesUI(userTitle: MutableList<String>){
+        val titleToViewMap = mapOf(
+            //프
+            "초보 프냥이" to binding.vf1,
+            "HTML 마스터" to binding.vf2,
+            "CSS 마법사" to binding.vf3,
+            "자바스크립트 냥이" to binding.vf4,
+            "프론트엔트 냥스터" to binding.vf5,
+            "프론트엔트 마에스트냥" to binding.vf6,
+            //백
+            "초보 백엔드 냥이" to binding.vb1,
+            "백엔드 탐험가 냥이" to binding.vb2,
+            "자바스크립트 백냥이" to binding.vb3,
+            "백엔드 엔지니어" to binding.vb4,
+            "백엔드의 냥스터" to binding.vb5,
+            "백엔드 마에스트냥" to binding.vb6
+        )
+        for(title in userTitle){
+            ViewOpacity(2,title)
+            setClickListener(titleToViewMap[title],title)
+            //binding.vf1,"초보 프냥이"
+        }
+    }
+
+    fun getName(): String {
+        val pref = getSharedPreferences("userName", 0)
+        return pref.getString("name", "").toString()
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item?.itemId) {
             android.R.id.home -> {
@@ -215,4 +245,8 @@ class ChangeTitleActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
+
 }
+//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 토큰
